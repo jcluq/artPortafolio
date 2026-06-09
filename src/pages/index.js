9100ea168d03f03f8f80436500186ad06857f3d9
jcluq <jluque@hfk-bremen.de>
2026-06-09 21:53:06 +0200

    sds

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,23 @@ const IndexPage = () => (
         
           <div className=" w-5/12 h-full wrap p-10 overflow-auto scrollbar border-l border-r border-black items-center align-middle">
             <div className=" mt-10 flex flex-col gap-10 text-justify ">
-              I am an artist and maker from Bogota, Colombia with a deep
+              Materials are tools, that we can use to shape and express the complexities that involve being alive. 
+              My practice is based on the research of the manipulation of materials and mediums, as there is a question abouit the incidence of time on our present existant that I want to share. 
+              Time is what shapes us from dust, and what directs us back to it. 
+
+              
+              <br></br>
+              <br></br>
+              
+              
+              
+              
+              
+          
+              I am an artist and maker from Bogota, Colombia , currently based in Germany, with a deep
               interest in installation, sculpture, physical computation and
               digital fabrication.<br></br>
-              <br></br>I like to work with perceptually interesting
-              technologies, objects and phenomena by figuring out ways to
-              transform them into experiences and contraptions that can express
-              what I consider special about the complexity of the world we live
-              in. <br></br>
-              <br></br>I also have experience doing introductory workshops,
-              using strategies to teach about art, electronics, programming and
-              digital fabrication in a horizontal way. I believe the sharing of
-              knowledge is the basis for a better future.
+              
             </div>
           </div>
         
